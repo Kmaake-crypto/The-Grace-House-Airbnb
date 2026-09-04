60f465e09f912f9e43c462463e46da88071573d0
Kmaake-crypto <koketsomaake295@gmail.com>
2026-09-04 14:19:16 +0200

    remove TailwindBase.css import, add directives to index.css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 import './index.css'
-import './TailwindBase.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
